000c9fa56625f9e499e8216f919de503230e5d1c
racisz? <72389680+KotTenRat@users.noreply.github.com>
2021-05-31 19:23:24 +0200

    Update BPS.java

--- BPS.java
+++ BPS.java
@@ -3,7 +3,8 @@ import net.minecraft.client.Minecraft;
 public class BPSCounter {
     static Minecraft mc = Minecraft.getMinecraft();
     public static void renderCounter(){
-        float bps = Math.round(mc.thePlayer.getDistance(mc.thePlayer.lastTickPosX, mc.thePlayer.posY, mc.thePlayer.lastTickPosZ) * 200) / 10f;
+        float rat = Minecraft.getMinecraft().timer.ticksPerSecond * Minecraft.getMinecraft().timer.timerSpeed;
+        float bps = mc.thePlayer.getDistance(mc.thePlayer.lastTickPosX, mc.thePlayer.posY, mc.thePlayer.lastTickPosZ) * rat;
         mc.fontRendererObj.drawStringWithShadow("BPS: " + bps, 3, 3, -1);
     }
 }
